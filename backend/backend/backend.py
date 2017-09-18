88965751a5c18ae3ee0b3ae8dff9b3c35df74ad9
Tobias Laundal totokaka <tobias@laundal.no>
2017-09-18 20:34:35 +0200

    Configurations for backend

--- backend/backend/backend.py
+++ backend/backend/backend.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.config.from_object("backend.default_settings")
+app.config.from_envvar("PPWB_CONFIG", silent=True)
+
 api = Api(app)
 
 class HelloWorld(Resource):
