564b617b1a60d8f62d962d1851c9a59d1a8c9f45
Tobias Laundal totokaka <tobias@laundal.no>
2017-09-20 22:04:34 +0200

    Begin work on layout

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,3 +1,16 @@
+
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.title = 'PowerfulPerms-Web';
+    config.map([
+      { route: '',        moduleId: 'no-category'},
+      { route: 'groups',  moduleId: 'groups',      name:'groups', title:'Groups' }
+    ]);
+
+    this.router = router;
+  }
 }
