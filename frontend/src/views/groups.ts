e6c34c5a7cdc33c320caac9ef3eee8003ed0d326
Tobias <mail@totokaka.io>
2017-09-25 23:10:30 +0200

    Add search for groups

--- frontend/src/views/groups.ts
+++ frontend/src/views/groups.ts
@@ -1,14 +1,34 @@
 import {autoinject} from 'aurelia-framework';
 import {Backend, Group} from '../lib/backend';
+import * as Fuse from 'fuse.js';
 
 @autoinject
 export class Groups {
 
   backend: Backend;
   groups: Array<Group>;
+  groups_view: Array<Group>;
+
+  fuse_options: Object;
+  fuse: Fuse;
+  search: String;
+
+  filter: (a: Group) => boolean = () => true;
+  sort: (a: Group, b: Group) => number = (a:Group, b:Group) => a < b ? -1 : 1;
 
   constructor(backend: Backend) {
     this.backend = backend;
+    this.fuse_options = {
+      shouldSort: true,
+      threshold: 0.6,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        "name"
+      ]
+    };
   }
 
   created() {
@@ -16,7 +36,15 @@ export class Groups {
     this.backend.getGroups()
       .then(groups => {
         $this.groups = groups;
+        $this.groups_view = $this.groups;
+        //$this.fuse = new Fuse($this.groups, $this.fuse_options);
       });
+  }
+
+  searchFor(search: string) {
+    // this.groups_view = this.fuse.search(search)
+    search = search.toLowerCase();
+    this.groups_view = this.groups.filter((group: Group) => group.name.toLowerCase().indexOf(search) !== -1);
   }
 
 }
