e1a2c8a187cda6bf0c9bad6892eab5c22e46f81e
Tobias <mail@totokaka.io>
2017-10-26 22:10:03 +0200

    Forward to login route when not logged in

--- frontend/src/lib/auth-service.ts
+++ frontend/src/lib/auth-service.ts
@@ -1,6 +1,7 @@
 import {autoinject} from 'aurelia-framework';
 import {HttpClient, json} from 'aurelia-fetch-client';
 
+@autoinject
 export class AuthService {
 
   endpoint: string = 'http://localhost:5000/';
@@ -21,11 +22,47 @@ export class AuthService {
     window.localStorage.setItem('powerfulperms-web.token', token);
   }
 
+  private generateHeader(username: string, password: string): string {
+    return btoa(`${username}:${password}`);
+  }
+
+  public isLoggedIn(): Promise<boolean> {
+    let $this = this;
+    return new Promise((resolve, reject) => {
+      let token = this.retrieveToken();
+      if (token == null) {
+        resolve(true);
+      }
+
+      $this.client.fetch('groups', {
+          method: 'GET',
+          headers: {'Authorization': this.getAuthorization()}
+        })
+        .then(response => {
+          resolve(response.ok);
+        })
+        .catch(err => {
+          resolve(false);
+        });
+    });
+  }
+
   public login(username: string, password: string) {
-    this.client.fetch('generate_token', {method: 'GET'})
-      .then(response => {
-        // TODO
+    let $this = this;
+    this.client.fetch('generate_token', {
+        method: 'GET',
+        headers: {'Authorization': this.generateHeader(username, password)}
+      })
+      .then(response => response.json())
+      .then(response => response.token)
+      .then(token => {
+        $this.storeToken(token);
+        return true;
       });
+  }
+
+  public getAuthorization(): string {
+    return this.generateHeader('token', this.retrieveToken());
   }
 
 }
